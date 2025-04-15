92376233c35c4883d95c573a0baa55efa2d9b04e
Sanaullah <sanaullah5099@gmail.com>
2025-04-15 11:24:21 +0500

    add speech to text library and make the floating action button workable for listening and recording

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:allen/colors.dart';
 import 'package:allen/feature_box.dart';
 import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +12,42 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final speechToText = SpeechToText();
+  String lastWords = '';
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    initSpeechToText();
+  }
+
+  Future<void> initSpeechToText() async {
+    await speechToText.initialize();
+    setState(() {});
+  }
+
+  Future<void> startListening() async {
+    await speechToText.listen(onResult: onSpeechResult);
+    setState(() {});
+  }
+
+  Future<void> stopListening() async {
+    await speechToText.stop();
+    setState(() {});
+  }
+
+  void onSpeechResult(SpeechRecognitionResult result) {
+    setState(() {
+      lastWords = result.recognizedWords;
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    speechToText.stop();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,98 +56,108 @@ class _HomePageState extends State<HomePage> {
         leading: const Icon(Icons.menu),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          Stack(
-            children: [
-              Center(
-                child: Container(
-                  height: 120,
-                  width: 120,
-                  margin: const EdgeInsets.only(top: 4),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Center(
+                  child: Container(
+                    height: 120,
+                    width: 120,
+                    margin: const EdgeInsets.only(top: 4),
+                    decoration: const BoxDecoration(
+                      color: Pallete.assistantCircleColor,
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 123,
                   decoration: const BoxDecoration(
-                    color: Pallete.assistantCircleColor,
                     shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/virtualAssistant.png'),
+                    ),
                   ),
+                )
+              ],
+            ),
+            //chat bubble
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 10,
+              ),
+              margin:
+                  const EdgeInsets.symmetric(horizontal: 40).copyWith(top: 30),
+              decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Pallete.borderColor,
+                  ),
+                  borderRadius: BorderRadius.circular(20).copyWith(
+                    topLeft: Radius.zero,
+                  )),
+              child: const Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.0),
+                child: Text(
+                  "Good Morning what task can I do for you?",
+                  style: TextStyle(
+                      color: Pallete.mainFontColor,
+                      fontSize: 25,
+                      fontFamily: 'Cera Pro'),
                 ),
               ),
-              Container(
-                height: 123,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                    image: AssetImage('assets/images/virtualAssistant.png'),
-                  ),
-                ),
-              )
-            ],
-          ),
-          //chat bubble
-          Container(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 20,
-              vertical: 10,
             ),
-            margin:
-                const EdgeInsets.symmetric(horizontal: 40).copyWith(top: 30),
-            decoration: BoxDecoration(
-                border: Border.all(
-                  color: Pallete.borderColor,
-                ),
-                borderRadius: BorderRadius.circular(20).copyWith(
-                  topLeft: Radius.zero,
-                )),
-            child: const Padding(
-              padding: EdgeInsets.symmetric(vertical: 10.0),
-              child: Text(
-                "Good Morning what task can I do for you?",
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: const EdgeInsets.all(10),
+              margin: const EdgeInsets.only(top: 10, left: 22),
+              child: const Text(
+                'Here are a few features',
                 style: TextStyle(
-                    color: Pallete.mainFontColor,
-                    fontSize: 25,
-                    fontFamily: 'Cera Pro'),
+                    fontFamily: 'Cera Pro',
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Pallete.mainFontColor),
               ),
             ),
-          ),
-          Container(
-            alignment: Alignment.centerLeft,
-            padding: const EdgeInsets.all(10),
-            margin: const EdgeInsets.only(top: 10, left: 22),
-            child: const Text(
-              'Here are a few features',
-              style: TextStyle(
-                  fontFamily: 'Cera Pro',
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Pallete.mainFontColor),
-            ),
-          ),
-          const Column(
-            children: [
-              FeatureBox(
-                headerText: 'ChatGPT',
-                descriptionText:
-                    "A smarter way to stay organised and informed with ChatGPT",
-                color: Pallete.firstSuggestionBoxColor,
-              ),
-              FeatureBox(
-                headerText: 'Dall-E',
-                descriptionText:
-                    "Get Inspired and stay creative with your personal assistance powered by Dall-E",
-                color: Pallete.secondSuggestionBoxColor,
-              ),
-              FeatureBox(
-                headerText: 'Smart Voice Assistance',
-                descriptionText:
-                    "Get the best of both worlds with a voice assistance powered by Dall-E and chatGPT",
-                color: Pallete.thirdSuggestionBoxColor,
-              ),
-            ],
-          )
-        ],
+            const Column(
+              children: [
+                FeatureBox(
+                  headerText: 'ChatGPT',
+                  descriptionText:
+                      "A smarter way to stay organised and informed with ChatGPT",
+                  color: Pallete.firstSuggestionBoxColor,
+                ),
+                FeatureBox(
+                  headerText: 'Dall-E',
+                  descriptionText:
+                      "Get Inspired and stay creative with your personal assistance powered by Dall-E",
+                  color: Pallete.secondSuggestionBoxColor,
+                ),
+                FeatureBox(
+                  headerText: 'Smart Voice Assistance',
+                  descriptionText:
+                      "Get the best of both worlds with a voice assistance powered by Dall-E and chatGPT",
+                  color: Pallete.thirdSuggestionBoxColor,
+                ),
+              ],
+            )
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Pallete.firstSuggestionBoxColor,
-        onPressed: () {},
+        onPressed: () async {
+          if (await speechToText.hasPermission && speechToText.isNotListening) {
+            await startListening();
+          } else if (speechToText.isListening) {
+            await stopListening();
+          } else {
+            initSpeechToText();
+          }
+        },
         child: const Icon(Icons.mic),
       ),
     );
