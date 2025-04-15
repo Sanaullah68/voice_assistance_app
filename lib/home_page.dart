2e57f99c8746c2809590371cec9e321e5c4d76b0
Sanaullah <sanaullah5099@gmail.com>
2025-04-15 17:53:34 +0500

    add animations

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:allen/colors.dart';
 import 'package:allen/feature_box.dart';
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:speech_to_text/speech_recognition_result.dart';
 import 'package:speech_to_text/speech_to_text.dart';
@@ -14,9 +15,10 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final speechToText = SpeechToText();
   String lastWords = '';
+  int start = 200;
+  int delay = 200;
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     initSpeechToText();
   }
@@ -52,7 +54,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Allen'),
+        title: BounceInDown(child: const Text('SM Tech')),
         leading: const Icon(Icons.menu),
         centerTitle: true,
       ),
@@ -72,75 +74,90 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ),
                 ),
-                Container(
-                  height: 123,
-                  decoration: const BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                      image: AssetImage('assets/images/virtualAssistant.png'),
+                ZoomIn(
+                  child: Container(
+                    height: 123,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/virtualAssistant.png'),
+                      ),
                     ),
                   ),
                 )
               ],
             ),
             //chat bubble
-            Container(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 20,
-                vertical: 10,
-              ),
-              margin:
-                  const EdgeInsets.symmetric(horizontal: 40).copyWith(top: 30),
-              decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Pallete.borderColor,
+            SlideInRight(
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 10,
+                ),
+                margin: const EdgeInsets.symmetric(horizontal: 40)
+                    .copyWith(top: 30),
+                decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Pallete.borderColor,
+                    ),
+                    borderRadius: BorderRadius.circular(20).copyWith(
+                      topLeft: Radius.zero,
+                    )),
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10.0),
+                  child: Text(
+                    "Good Morning what task can I do for you?",
+                    style: TextStyle(
+                        color: Pallete.mainFontColor,
+                        fontSize: 25,
+                        fontFamily: 'Cera Pro'),
                   ),
-                  borderRadius: BorderRadius.circular(20).copyWith(
-                    topLeft: Radius.zero,
-                  )),
-              child: const Padding(
-                padding: EdgeInsets.symmetric(vertical: 10.0),
-                child: Text(
-                  "Good Morning what task can I do for you?",
+                ),
+              ),
+            ),
+            SlideInLeft(
+              child: Container(
+                alignment: Alignment.centerLeft,
+                padding: const EdgeInsets.all(10),
+                margin: const EdgeInsets.only(top: 10, left: 22),
+                child: const Text(
+                  'Here are a few features',
                   style: TextStyle(
-                      color: Pallete.mainFontColor,
-                      fontSize: 25,
-                      fontFamily: 'Cera Pro'),
+                      fontFamily: 'Cera Pro',
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Pallete.mainFontColor),
                 ),
               ),
             ),
-            Container(
-              alignment: Alignment.centerLeft,
-              padding: const EdgeInsets.all(10),
-              margin: const EdgeInsets.only(top: 10, left: 22),
-              child: const Text(
-                'Here are a few features',
-                style: TextStyle(
-                    fontFamily: 'Cera Pro',
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Pallete.mainFontColor),
-              ),
-            ),
-            const Column(
+            Column(
               children: [
-                FeatureBox(
-                  headerText: 'ChatGPT',
-                  descriptionText:
-                      "A smarter way to stay organised and informed with ChatGPT",
-                  color: Pallete.firstSuggestionBoxColor,
+                SlideInLeft(
+                  delay: Duration(microseconds: start),
+                  child: const FeatureBox(
+                    headerText: 'ChatGPT',
+                    descriptionText:
+                        "A smarter way to stay organised and informed with ChatGPT",
+                    color: Pallete.firstSuggestionBoxColor,
+                  ),
                 ),
-                FeatureBox(
-                  headerText: 'Dall-E',
-                  descriptionText:
-                      "Get Inspired and stay creative with your personal assistance powered by Dall-E",
-                  color: Pallete.secondSuggestionBoxColor,
+                SlideInLeft(
+                  delay: Duration(microseconds: start + delay),
+                  child: const FeatureBox(
+                    headerText: 'Dall-E',
+                    descriptionText:
+                        "Get Inspired and stay creative with your personal assistance powered by Dall-E",
+                    color: Pallete.secondSuggestionBoxColor,
+                  ),
                 ),
-                FeatureBox(
-                  headerText: 'Smart Voice Assistance',
-                  descriptionText:
-                      "Get the best of both worlds with a voice assistance powered by Dall-E and chatGPT",
-                  color: Pallete.thirdSuggestionBoxColor,
+                SlideInLeft(
+                  delay: Duration(milliseconds: start + 2 * delay),
+                  child: const FeatureBox(
+                    headerText: 'Smart Voice Assistance',
+                    descriptionText:
+                        "Get the best of both worlds with a voice assistance powered by Dall-E and chatGPT",
+                    color: Pallete.thirdSuggestionBoxColor,
+                  ),
                 ),
               ],
             )
